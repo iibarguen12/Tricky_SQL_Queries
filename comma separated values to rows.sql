682742496554db41a8ee678128041c7f76c7e6a7
Iries David Ibarguen Ruiz <56435535+iibarguen12@users.noreply.github.com>
2021-07-30 00:02:08 +0200

    Delete spaces and formatting

--- comma separated values to rows.sql
+++ comma separated values to rows.sql
@@ -25,7 +25,7 @@ select regexp_replace(cols,'^ | $','') clean_cols
 --For this one, use the same approach as before. First split the prhase in several rows, but insted of usgin comma use space
 --Then, add a row_nunmber column to enumerate each row (word)
 --Finally, use the listagg function to join all the rows into a single one separated by space, and specify descending order to reverse the rows (words)
-```sql
+
 with 
 input_value    as (select 'hello world here!' val 
                      from dual),
@@ -38,7 +38,6 @@ rows_to_prhase as (select listagg(word_col,' ')
                      from prhase_to_rows)                   
 select prhase 
   from rows_to_prhase;
-```
 
 --3) Given two rows of separated comma values, convert it to multiple rows:
 --input_value: 
@@ -55,7 +54,7 @@ select prhase
 --For this one, is necessary to create a different level for each row, due the first row has just 2 values and the second 4 values
 --To achieve this, is possible to create a inline view and use the LATERAL clause in order to reference the table on the left
 --After that is easy to follow the same pattern as exercise 1 and 2
-```sql
+
 with 
 input_value as (select 'David,Estefania' as various_names from dual
                 union all
@@ -63,12 +62,11 @@ input_value as (select 'David,Estefania' as various_names from dual
 select regexp_substr(input_value.various_names,'[^,]+',1,lvl) single_name
 from input_value, 
 lateral(select level lvl from dual connect by level <= regexp_count(input_value.various_names,'[^,]+'));
-```
 
 --The same result can be achieved in several ways, for example instead of using LATERAL, just create a level for all the rows indiscriminately
 --and then get only the unique ones with the DISTINCT clause, but be aware that in these cases the generated rows can increase exponentially 
 --as well that you will lose equal names in different rows
-```sql
+
 with 
 input_value as (select 'David,Estefania' as various_names from dual
                 union all
@@ -77,4 +75,4 @@ select distinct regexp_substr(input_value.various_names,'[^,]+',1,level) single_
 from input_value 
 connect by regexp_substr(various_names,'[^,]+',1,level) is not null
 order by single_name;
-```
+
